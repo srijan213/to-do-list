3d0ff5ee4d6dd7d3448548ec2ae762374b3e1b17
Srijan Manandhar <I82646@d2hs.com>
2019-03-26 18:39:20 +0545

    Add Delete and Done Functionality added | Css: Padding to Input Field

--- javascript/app.js
+++ javascript/app.js
@@ -1,23 +1,45 @@
 $(document).ready(function() {
   let userInput = "";
-
+  let listItemNumber = 0;
   function userInputFetcher() {
     userInput = $("#input").val();
   }
-  function listItemAdder(itemDesc) {
-    let listItem = $(`<div class="list">
-    <i class="far fa-square" class="bullet--icon"></i>
-    <!-- <i class="far fa-check-square" class="bullet__check--icon"></i> -->
+  function listItemAdder(itemDesc, listItemNumber) {
+    let listItem = $(`<div class="list" id="list-item-${listItemNumber}">
+    <i class="far fa-square"></i>
+    <!-- <i class="far fa-check-square"></i> -->
     <span class="item--desc">${itemDesc}</span>
-    <i class="far fa-check-circle" class="done--icon"></i>
-    <i class="far fa-edit" class="edit--icon"></i>
-    <i class="far fa-trash-alt" class="delete--icon"></i>
+    <span class="done--icon"><i class="far fa-check-circle"></i></span>
+    <span class="edit--icon"><i class="far fa-edit"></i></span>
+    <span class="delete--icon"><i class="far fa-trash-alt"></i></span>
   </div>`);
     $(".container").append(listItem);
   }
+
   $("#add--button").click(function() {
     userInputFetcher();
     if (userInput == "") alert("Task cannot be empty");
-    else listItemAdder(userInput);
+    else {
+      listItemNumber += 1;
+      listItemAdder(userInput, listItemNumber);
+    }
+  });
+
+  $(document).on("click", ".delete--icon", function() {
+    $(this)
+      .parent()
+      .remove();
+  });
+
+  $(document).on("click", ".done--icon", function() {
+    $(this)
+      .parent()
+      .children(".item--desc")
+      .css("text-decoration", "line-through");
+
+    $(this)
+      .parent()
+      .children(":first")
+      .attr("class", "far fa-check-square");
   });
 });
